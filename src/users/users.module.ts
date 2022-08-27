a8cb54068218f483265d31fcfca154bf510b25c5
Lev Salmin <udodirc@yahoo.com>
2022-08-27 12:22:11 +0600

    feat(guards, controllers, modules): Create user guard fot authorization.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './users.model';
@@ -6,11 +6,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from '../roles/roles.model';
 import { UserRoles } from '../roles/user-roles.model';
 import { RolesModule } from '../roles/roles.module';
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
-  exports:[UsersService, UsersModule]
+  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule, forwardRef(() => AuthModule)],
+  exports: [UsersService]
 })
 export class UsersModule {}
